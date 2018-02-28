b121584dd773734deb15efd4cddb67506e2d56f9
Kelvin Porter <kporter@protonmail.com>
2018-02-28 17:05:03 -0500

    sanders: BoardConfig.mk: use cryptfs_hw in device dir

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,6 +140,7 @@ ENABLE_CPUSETS := true
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
 
 # Enable dexpreopt to speed boot time
 WITH_DEXPREOPT := true
