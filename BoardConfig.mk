f6656c31a2a6b3fdb901ca969002a4c005b350bf
Kelvin Porter <kporter@protonmail.com>
2018-03-20 17:38:39 -0400

    sanders: update gcc toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,8 +99,8 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 
 #
 # Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-linaro-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
